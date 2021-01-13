f31c6ebe6c0735617f756e106af00035e8535cad
MichalMiekina <47924236+MichalMiekina@users.noreply.github.com>
2021-01-13 13:15:59 +0100

    reading location

--- wtyczka/stats.js
+++ wtyczka/stats.js
@@ -1,11 +1,21 @@
 
-hl.addEventListener("click",sayxd);
+hl.addEventListener('click',function(e){
+    nodeName = e.target.nodeName;
+    e.target.id ? id = e.target.id : id = null;
+    classes = e.target.className.split(' ');
+    console.log(window.location.hostname+nodeName + id + classes);
+});
 
-function sayxd(){
-    console.log("XD");
-}
+// function sayxd(e){
+//     e=e || window.event;
+//     console.log(e.target);
+// }
 
-// website.onclick=function(event){
-// // 	console.log("XD");
-// // }
+
+// document.addEventListener('click', function(e) {
+//     e = e || window.event;
+//     var target = e.target || e.srcElement,
+//     text = target.textContent || target.innerText;   
+//     console.log(target);
+// }, false);
 

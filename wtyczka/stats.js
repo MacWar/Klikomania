7b28af1b7ea9a0b5dbd0c3cfbb1b486d4b07c188
MichalMiekina <47924236+MichalMiekina@users.noreply.github.com>
2021-01-19 00:22:08 +0100

    hrefElements

--- wtyczka/stats.js
+++ wtyczka/stats.js
@@ -1,11 +1,28 @@
 
+window.onload = function() {
+
+    // var hrefElements = document.querySelectorAll('a:not([href=""])');
+
+    // console.log(hrefElements);
+
+
+
+    document.querySelectorAll('a:not([href=""])').forEach(item => {
+        item.addEventListener('click', event => {
+        
+            console.log(item);
+        })
+    })
+};
+
+
 hl.addEventListener('click',function(e){
     nodeName = e.target.nodeName;
     e.target.id ? id = e.target.id : id = null;
     classes = e.target.className.split(' ');
     console.log(window.location.hostname+nodeName + id + classes);
     
-    console.log(e.target.href);
+
 
 
     // var href=e.target.getAttribute("href");

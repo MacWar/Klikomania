97c9372b73b1d53cd2fbc67450c4a0083b178de0
Namtrak <mateosxxx@gmail.com>
2021-01-18 23:58:55 +0100

    Wersja 2

--- wtyczka/stats.js
+++ wtyczka/stats.js
@@ -20,6 +20,18 @@
 // }, false);
 
 
+var cookie = navigator.cookieEnabled;
+var platform = navigator.platform;
+var language = navigator.language;
+var width = screen.width;
+var height = screen.height;
+    
+console.log("Cookie is enabled: " + cookie);
+console.log("User platform is: " + platform);
+console.log("User language is: " + language);
+console.log("User screen is " + width + " width");
+console.log("User screen is " + height + " height");
+
 document.querySelector("body").addEventListener("click", (event) => {
     /*const isButton = event.target.nodeName === 'BUTTON';
 
@@ -31,6 +43,8 @@ document.querySelector("body").addEventListener("click", (event) => {
     var btn_class = event.target.className;
     var btn_id = event.target.id;
     var btn_parent = event.target.parentNode.nodeName;
+    var date = new Date();
+    var time = date.toLocaleTimeString();
 
     if (btn_class == "") {
         btn_class = null;
@@ -39,8 +53,12 @@ document.querySelector("body").addEventListener("click", (event) => {
         btn_id = null;
     }
 
+    
+
     console.log("The name of clicked object is:" + btn_name);
     console.log("The class of clicked object is:" + btn_class);
     console.log("The id of clicked object is:" + btn_id);
     console.log("The name of clicked object parent is:" + btn_parent);
+    console.log("The time of clicked object is: " + time);
+
 })

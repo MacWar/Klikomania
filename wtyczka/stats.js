e3cb0d5eb925ee9e03480427e02dccec0653d140
Namtrak <mateosxxx@gmail.com>
2021-01-18 18:09:55 +0100

    Wersja alpha logow w konsoli

--- wtyczka/stats.js
+++ wtyczka/stats.js
@@ -1,10 +1,10 @@
 
-hl.addEventListener('click',function(e){
+/*hl.addEventListener('click',function(e){
     nodeName = e.target.nodeName;
     e.target.id ? id = e.target.id : id = null;
     classes = e.target.className.split(' ');
     console.log(window.location.hostname+nodeName + id + classes);
-});
+});*/
 
 // function sayxd(e){
 //     e=e || window.event;
@@ -19,3 +19,28 @@ hl.addEventListener('click',function(e){
 //     console.log(target);
 // }, false);
 
+
+document.querySelector("body").addEventListener("click", (event) => {
+    /*const isButton = event.target.nodeName === 'BUTTON';
+
+    if (!isButton) {
+        return;
+    }*/
+
+    var btn_name = event.target.nodeName;
+    var btn_class = event.target.className;
+    var btn_id = event.target.id;
+    var btn_parent = event.target.parentNode.nodeName;
+
+    if (btn_class == "") {
+        btn_class = null;
+    } 
+    else if (btn_id == "") {
+        btn_id = null;
+    }
+
+    console.log("The name of clicked object is:" + btn_name);
+    console.log("The class of clicked object is:" + btn_class);
+    console.log("The id of clicked object is:" + btn_id);
+    console.log("The name of clicked object parent is:" + btn_parent);
+})

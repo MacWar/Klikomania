1d7826b7821e2ae681acad39b87dde675113bcb9
MacWar <61847651+MacWar@users.noreply.github.com>
2021-01-20 20:46:42 +0100

    chcnge id of heatmap button

--- wtyczka/popup.js
+++ wtyczka/popup.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded',function(){
-    document.querySelector('button').addEventListener('click',onclick,false)
+    document.getElementById('heatmapButton').addEventListener('click',onclick,false)
     function onclick(){
         chrome.tabs.query({currentWindow: true,active:true},
             function(tabs){

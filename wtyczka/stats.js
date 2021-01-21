15070facedd4bcfd895f7f5714506d07ed9fb405
Namtrak <mateosxxx@gmail.com>
2021-01-21 15:39:39 +0100

    ok

--- wtyczka/stats.js
+++ wtyczka/stats.js
@@ -15,34 +15,6 @@ function clickCounter() {
 }
   
 window.onload = function () {
-<<<<<<< HEAD
-    /*if(window.location.pathname==localStorage.leavingPageDataTMP.leavingPageData.pathname) localStorage.leavingPageDataTMP=null;
-
-    if(localStorage.leavingPageDataTMP!=null){
-        download("leavingStats.json",localStorage.leavingPageDataTMP);
-        localStorage.leavingPageDataTMP=null;
-    }*/
-
-    var hrefElements = document.querySelectorAll('a[href^="http://"], a[href^="https://"], a[href^="/"]');
-    
-    console.log(hrefElements);
-
-    window.addEventListener('beforeunload', function (e) {
-        // Cancel the event
-        //e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
-        // Chrome requires returnValue to be set
-        //lastRemember();
-        
-        leavingPageData = window.location;
-        leavingPageDataJSON = {
-            leavingPageData : leavingPageData
-        }
-        localStorage.leavingPageDataTMP = window.location;
-        e.returnValue = 'dgf';
-      });
-      
-=======
-    checkCookie();
 
 
     // window.addEventListener('beforeunload', function (e) {
@@ -54,7 +26,6 @@ window.onload = function () {
     //     download("leavingStats.json",JSON.stringify(result));
     //   });
     // }
->>>>>>> f3b18b3b356addb44d840493c365cbe44af98acc
 
   fetch("https://api.astroip.co/?api_key=b500ad2b-d013-4c56-ab63-05262680f030")
   .then((response) => response.json()) 
@@ -78,7 +49,6 @@ document.querySelectorAll('a[href^="http://"], a[href^="https://"], a[href^="/"]
             userdata : userdata,
             location: window.location,
             hrefItem: clickedHrefItem,
-<<<<<<< HEAD
             event: event,
             exitsNumber: localStorage.clickcount
         };
@@ -90,11 +60,6 @@ document.querySelectorAll('a[href^="http://"], a[href^="https://"], a[href^="/"]
             dataa = JSON.parse(result.dane);
             download(time + " " + pagelink + " stats.json", JSON.stringify(dataa));
         });
-=======
-            event: event
-        };
-        download("hrefStats.json",JSON.stringify(filesDataJSON));
->>>>>>> f3b18b3b356addb44d840493c365cbe44af98acc
 
         var url = chrome.runtime.getURL('stats.json');
         fetch(url)
@@ -102,20 +67,6 @@ document.querySelectorAll('a[href^="http://"], a[href^="https://"], a[href^="/"]
             .then((json) => console.log(json));          
     })
 })
-<<<<<<< HEAD
-
-function lastRemember(){
-    var pageLeavingData = window;
-
-    pageLeavingDataJSON = {
-        windowData : pageLeavingData
-    }
-
-    download("stats.json",JSON.stringify(pageLeavingDataJSON));
-}
-
-=======
->>>>>>> f3b18b3b356addb44d840493c365cbe44af98acc
 function download(filename, text) {
     let temp = document.createElement('a');
     temp.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
@@ -124,50 +75,8 @@ function download(filename, text) {
     document.body.appendChild(temp);
     temp.click();
     document.body.removeChild(temp);
-<<<<<<< HEAD
   }
 
-=======
-}
-
-
-//                ######################    Clicks data & leaving pages data
-
-function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
-  
-  function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
-  
-  function checkCookie() {
-    var user=getCookie("username");
-    if (user != "") {
-      alert("Welcome again " + user);
-    } else {
-       user = prompt("Please enter your name:","");
-       if (user != "" && user != null) {
-         setCookie("username", user, 30);
-       }
-    }
-  }
->>>>>>> f3b18b3b356addb44d840493c365cbe44af98acc
 
 var cookie = navigator.cookieEnabled;
 var platform = navigator.platform;
@@ -232,23 +141,3 @@ document.querySelector("body").addEventListener("click", (event) => {
     console.log("The time of clicked object is: " + time);
     console.log("Page link is: " + page_link);
 })
-
-/*hl.addEventListener('click',function(e){
-/*hl.addEventListener('click',function(e){
-    nodeName = e.target.nodeName;
-    e.target.id ? id = e.target.id : id = null;
-    classes = e.target.className.split(' ');
-    console.log(window.location.hostname+nodeName + id + classes);
-    
-
-});
-
-
-    // var href=e.target.getAttribute("href");
-
-    // console.log(href);
-    // if(href!= null){
-    //     href = href.split(".");
-    //     console.log(href);
-    // }
-});*/

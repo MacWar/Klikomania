7fc372c14fce90cd8b97ffd130b534c34c9eb55d
Namtrak <mateosxxx@gmail.com>
2021-01-21 11:12:47 +0100

    Update stats.js

--- wtyczka/stats.js
+++ wtyczka/stats.js
@@ -1,5 +1,6 @@
 var userdata;
-window.onload = function() {
+window.onload = function () {
+    checkCookie();
 
     if(window.location.pathname==localStorage.leavingPageDataTMP.leavingPageData.pathname) localStorage.leavingPageDataTMP=null;
 
@@ -95,7 +96,40 @@ hl.addEventListener('click',function(e){
     // }
 });*/
 
-
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    var user=getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+         setCookie("username", user, 30);
+       }
+    }
+  }
 
 var cookie = navigator.cookieEnabled;
 var platform = navigator.platform;
